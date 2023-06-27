aba172283b4eb826b79e0e5bfeefb96a1ecac556
avi <avi.ruiz@proton.me>
2023-06-27 00:12:33 -0600

    views for menu serializer created

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    serializer_class = serializers.MenuSerializer
+    queryset = models.MenuTable.objects.all()
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.MenuSerializer
+    queryset = models.MenuTable.objects.all()
