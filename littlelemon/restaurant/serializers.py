6bc51def4d305269c13b2705a8b4a442d7f70935
avi <avi.ruiz@proton.me>
2023-06-27 00:15:49 -0600

    include view for serializers in urlspatterns, add serializer for user

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["url", "username", "email", "groups"]
 
 
 class MenuSerializer(serializers.ModelSerializer):
