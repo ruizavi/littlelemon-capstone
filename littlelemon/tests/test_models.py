c1d463d47470236e003daeafe9f6a47a5fc47a2b
Avi <avi.ruiz@proton.me>
2023-06-27 11:14:44 -0600

    separated content for index.html in fragments

--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -6,4 +6,6 @@ class MenuItemTest(TestCase):
     def test_get_item(self):
         item = models.MenuItem.objects.create(
             title='IceCream', price=80, inventory=100)
-        self.assertEqual(item, "IceCream : 80")
+
+        itemstr = item.get_item()
+        self.assertEqual(itemstr, "IceCream : 80")
